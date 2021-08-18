ce9a18e084a906cd8754d2ce0ea9eb3443dc65c8
Struggleton! <thatstrugglethough@gmail.com>
2021-08-17 22:28:24 -0400

    Remove all unused using statements

--- DelfinovinUI/MainWindow.xaml.cs
+++ DelfinovinUI/MainWindow.xaml.cs
@@ -1,27 +1,15 @@
 ﻿using LibUsbDotNet;
 using LibUsbDotNet.DeviceNotify;
 using LibUsbDotNet.Main;
-using MaterialDesignThemes.Wpf.Transitions;
 using Nefarius.ViGEm.Client;
 using Nefarius.ViGEm.Client.Exceptions;
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using static System.Windows.Forms.ListView;
 
 
 namespace DelfinovinUI
