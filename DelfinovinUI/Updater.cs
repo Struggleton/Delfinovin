c771d35d446d5341d36f49a9f8b4a9937272d5f8
Struggleton! <thatstrugglethough@gmail.com>
2021-10-14 21:10:23 -0400

    Remove unused usings

--- DelfinovinUI/Updater.cs
+++ DelfinovinUI/Updater.cs
@@ -1,14 +1,10 @@
 ﻿using Octokit;
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.IO.Compression;
-using System.Linq;
 using System.Net;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace DelfinovinUI
 {
