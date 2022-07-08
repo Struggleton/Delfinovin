cedbaf04a2cdc242310767c523d0cf250b9944d2
Struggleton <thatstrugglethough@gmail.com>
2022-07-08 11:06:38 -0400

    Separate Delfinovin updater into separate project/executable

--- DelfinovinUI/Updater.cs
+++ DelfinovinUI/Updater.cs
@@ -12,108 +12,62 @@ namespace DelfinovinUI
     {
         public static async void CheckCurrentRelease(bool silent)
         {
-            try
+            // Do this so we don't get rejected by the API
+            var client = new GitHubClient(new ProductHeaderValue("DelfinovinUI"));
+
+            // Access the main repository
+            var releases = await client.Repository.Release.GetAll("Struggleton", "Delfinovin");
+
+            // Get the most current release
+            var latest = releases[0];
+
+            // Create a version object. We need to strip the nonnumeric chars from the
+            // Github versioning. 
+            Version githubVersion = new Version(Regex.Replace(latest.TagName, "[^0-9.]", ""));
+
+            // Get the application's current version and compare it to the github version.
+            Version localVersion = typeof(Updater).Assembly.GetName().Version;
+            int versionComparison = localVersion.CompareTo(githubVersion);
+
+            // The version on GitHub is more up to date than this local release.
+            if (versionComparison < 0)
             {
-                // Do this so we don't get rejected by the API
-                var client = new GitHubClient(new ProductHeaderValue("DelfinovinUI"));
+                string updateText = string.Format("There is a newer version of Delfinovin (Current is v{0}, Newer is v{1}) available for download!\n"
+                                            + "Would you like to?", localVersion.ToString(), githubVersion.ToString());
 
-                // Access the main repository
-                var releases = await client.Repository.Release.GetAll("Struggleton", "Delfinovin");
+                MessageWindow updateWindow = new MessageWindow(updateText, true, true, "Yes", "No");
+                updateWindow.ShowDialog();
 
-                // Get the most current release
-                var latest = releases[0];
 
-                // Create a version object. We need to strip the nonnumeric chars from the
-                // Github versioning. 
-                Version githubVersion = new Version(Regex.Replace(latest.TagName, "[^0-9.]", ""));
-
-                // Get the application's current version and compare it to the github version.
-                Version localVersion = typeof(Updater).Assembly.GetName().Version;
-                int versionComparison = localVersion.CompareTo(githubVersion);
-
-                // The version on GitHub is more up to date than this local release.
-                if (versionComparison < 0)
+                if (updateWindow.Result == WindowResult.OK)
                 {
-                    string updateText = string.Format("There is a newer version of Delfinovin (Current is v{0}, Newer is v{1}) available for download!\n"
-                                                + "Would you like to?", localVersion.ToString(), githubVersion.ToString());
+                    // TO-DO: pull this from a separate repository, so it can't be missing
+                    try
+                    {
+                        Process.Start("DelfinovinUpdater.exe");
+                    }
 
-                    MessageWindow updateWindow = new MessageWindow(updateText, true, true, "Yes", "No");
+                    catch (Exception ex)
+                    {
+                        Process.Start("https://github.com/Struggleton/Delfinovin/releases/latest/");
+
+                        MessageWindow errorWindow = new MessageWindow("Missing updater. Navigating to the github releases page.", true);
+                        errorWindow.Show();  
+                    }
+                }
+
+            }
+
+            // The local version is more up to date or equal to the newest version on Github
+            else
+            {
+                // Supress the message window 
+                if (!silent)
+                {
+                    MessageWindow updateWindow = new MessageWindow("You are on the most up to date version!");
                     updateWindow.ShowDialog();
-
-
-                    if (updateWindow.Result == WindowResult.OK)
-                    {
-                        DownloadAndUnpackNewRelease();
-                    }
-
-                }
-
-                // The local version is more up to date or equal to the newest version on Github
-                else
-                {
-                    // Supress the message window 
-                    if (!silent)
-                    {
-                        MessageWindow updateWindow = new MessageWindow("You are on the most up to date version!");
-                        updateWindow.ShowDialog();
-                    }
                 }
             }
-
-            catch (Exception e)
-            {
-
-            }
-        }
-
-        private static async void DownloadAndUnpackNewRelease()
-        {
-            // Create a WebClient object and download the newest release.
-            using (WebClient webClient = new WebClient())
-            {
-                await webClient.DownloadFileTaskAsync(new Uri("https://github.com/Struggleton/Delfinovin/releases/latest/download/Delfinovin.zip"), "DelfinovinLatest.zip");
-            }
-
-            // If the directory for updates exists, delete it.
-            if (Directory.Exists("update"))
-                Directory.Delete("update", true);
-
-            // Extract files from the downloaded zip to an update folder
-            ZipFile.ExtractToDirectory("DelfinovinLatest.zip", "update");
-
-            string[] updateScript =
-            {
-                // Disable text writing to the console
-                "@echo off",
-
-                // Kill the currently running process of Delfinovin
-                "taskkill /f /im DelfinovinUI.exe > NUL", 
-
-                 // Wait 1 second for the process to close 
-                "TIMEOUT /t 1 /nobreak > NUL",
-
-                // Make a copy of the settings file before overwriting it
-                "copy \"settings.txt\" \"settings.bak\"",
-
-                // Move the update files from the update folder to the working dir
-                "move /y \"update\\*.*\"",
-
-                // Bring settings file back
-                "move /y \"settings.bak\" \"settings.txt\"",
-
-                // Run Delfinovin
-                "start DelfinovinUI.exe",
-
-                // Delete the update archive
-                "del /Q  \"DelfinovinLatest.zip\"",
-
-                // Delete the update script
-                "del /Q  \"updateScript.bat\""
-            };
-
-            // Write the script to a bat file and run it
-            File.WriteAllLines("updateScript.bat", updateScript);
-            Process.Start("updateScript.bat");
         }
     }
 }
---
+++ DelfinovinUpdater/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelfinovinUpdater
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                DeleteWorkspace();
+
+                // Create a WebClient object and download the newest release.
+                using (WebClient webClient = new WebClient())
+                {
+                    Console.WriteLine("Downloading latest Delfinovin release.");
+                    await webClient.DownloadFileTaskAsync(new Uri("https://github.com/Struggleton/Delfinovin/releases/latest/download/Delfinovin.zip"), "DelfinovinLatest.zip");
+                }
+
+                // Check to see if there is a current config
+                // if so, back it up.
+                if (File.Exists("settings.txt"))
+                {
+                    // Backup settings config
+                    Console.WriteLine("Backing up current config.");
+                    File.Move("settings.txt", "settings.bak");
+                }
+
+
+                // Get the current running process of Delfinovin and kill them.
+                Console.WriteLine("Closing any current instances of Delfinovin...");
+                Process[] processes = Process.GetProcessesByName("DelfinovinUI");
+                if (processes.Length > 0)
+                {
+                    foreach (Process p in processes)
+                        p.Kill();
+                }
+
+                UnpackRelease();
+
+                // Check to see if previous settings exist.
+                // if so, pull them in.
+                if (File.Exists("settings.bak"))
+                    File.Delete("settings.txt");
+
+                File.Move("settings.bak", "settings.txt");
+
+                // Start Delfinovin
+                Console.WriteLine("Starting Delfinovin.");
+                Process.Start("DelfinovinUI.exe");
+
+                DeleteWorkspace();
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine("Could not download latest release. Check your internet connection.");
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        static void DeleteWorkspace()
+        {
+            // If the directory for updates exists, delete it.
+            if (Directory.Exists("update"))
+                Directory.Delete("update", true);
+
+            if (File.Exists("DelfinovinLatest.zip"))
+                File.Delete("DelfinovinLatest.zip");
+        }
+
+        static void BackupConfig()
+        { 
+        }
+
+        static void UnpackRelease()
+        {
+            // Extract files from the downloaded zip to an update folder
+            Console.WriteLine("Unpacking release...");
+            ZipFile.ExtractToDirectory("DelfinovinLatest.zip", "update");
+
+
+            // Get all the files from the update directory and try to move them
+            string[] files = Directory.GetFiles("update");
+            foreach (string file in files)
+            {
+                try
+                {
+                    File.Copy(file, Path.GetFileName(file), true);
+                }
+
+                // 
+                catch (Exception ex)
+                {
+                    continue;
+                }
+
+            }
+        }
+    }
+}
