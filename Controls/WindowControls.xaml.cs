796f03bde6d2275b577714efe841a915fe8b8591
Struggleton! <thatstrugglethough@gmail.com>
2022-10-22 11:31:55 -0400

    Add reusable window controls (WindowControls)

---
+++ Controls/WindowControls.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Delfinovin.Controls
+{
+    /// <summary>
+    /// Interaction logic for WindowControls.xaml
+    /// </summary>
+    public partial class WindowControls : UserControl
+    {
+        private Window ParentWindow { get; set; }
+
+        public Visibility MinimizeButtonVisibility { get; set; }
+        public Visibility CloseButtonVisibility { get; set; }
+
+        public WindowControls()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+        }
+
+        private void WindowControlsLoaded(object sender, RoutedEventArgs e)
+        {
+            // Get the parent window so we can manipulate it
+            ParentWindow = Window.GetWindow(this);
+        }
+
+        private void windowClose_Click(object sender, RoutedEventArgs e)
+        {
+            if (ParentWindow != null)
+                ParentWindow.Close();
+        }
+
+        private void windowMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            if (ParentWindow != null)
+                ParentWindow.WindowState = WindowState.Minimized;
+        }
+    }
+}
