5ff2b560e7527a56d67137d4fabeab8b0326c091
Struggleton! <thatstrugglethough@gmail.com>
2022-10-24 00:57:43 -0400

    Rename class to ControlExtensions in preparation for separate Extensions class

---
+++ Controls/ControlExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Delfinovin
+{
+    public static class ControlExtensions
+    {
+        /// <summary>
+        /// Gets the X/Y coordinates above a control.
+        /// </summary>
+        /// <param name="baseControl">The control to spawn relative to.</param>
+        /// <param name="spawningControl">The control being spawned.</param>
+        /// <returns>Point, the location above the baseControl.</returns>
+        public static Point GetScreenSpawnPosition(Control baseControl, Control spawningControl)
+        {
+            // Get the current location on screen the main control is at.
+            Point locationFromScreen = baseControl.PointToScreen(new Point(baseControl.ActualWidth, 0));
+
+
+            // Get position above the control using the spawningControl's height/width.
+            Point spawningLocation = new Point(0, 0);
+            spawningLocation.X = locationFromScreen.X - spawningControl.Width;
+            spawningLocation.Y = locationFromScreen.Y - spawningControl.Height;
+
+            return spawningLocation;
+        }
+    }
+}
